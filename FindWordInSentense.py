ad54f0e14f3852d711ffa700a272c848d51b3f7b
Martin <lipsergey@gmail.com>
2018-03-16 14:34:23 +0300

    Initial

---
+++ FindWordInSentense.py
@@ -0,0 +1,35 @@
+import pickle
+import nltk
+#from nltk.corpus import stopwords
+from pathlib import Path
+#from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
+#import re
+#import functools
+
+#nltk.download('punkt')
+
+
+text_file_name = r"Alice.txt"
+sentence_list_file = Path(text_file_name.replace('.txt', '.pickle').replace('.TXT', '.pickle'))
+
+extra_abbreviations = {'dr', 'vs', 'mr', 'mrs', 'prof', 'inc', 'i.e'}
+sentence_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+sentence_tokenizer._params.abbrev_types.update(extra_abbreviations)
+
+
+if sentence_list_file.is_file():
+    with open(sentence_list_file, 'rb') as text_file:
+        sentences=pickle.load(text_file)
+        print('LOADING pickle')
+else:
+    with open(text_file_name, 'rt') as text_file:     ##, encoding='1251'
+        sentences = sentence_tokenizer.tokenize(text_file.read(), realign_boundaries=True)
+        print(len(sentences))
+
+        with open(sentence_list_file, 'wb') as text_file:
+            pickle.dump(sentences, text_file, pickle.HIGHEST_PROTOCOL)
+
+
+for sentence in sentences:
+    if ' rather' in sentence:
+        print(sentence)
